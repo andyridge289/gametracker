48519ff738c544dd145e91cd0c9e04cc8d17c67e
Andy <andy@Andys-MacBook-Pro.local>
2021-01-10 18:24:49 +0000

    Enable jetpack compose

--- app/src/main/java/com/andy/games/MainActivity.kt
+++ app/src/main/java/com/andy/games/MainActivity.kt
@@ -6,28 +6,17 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.andy.games.ui.main.SectionsPagerAdapter
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.setContent
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        setContent {
+            Text(text = "Hello")
         }
     }
 }
