646ab2389d4321dca9fbb7f87df6dbd9858ade6d
Andy <andy@Andys-MacBook-Pro.local>
2021-01-10 22:28:17 +0000

    Revert "Enable jetpack compose"
    This reverts commit 48519ff738c544dd145e91cd0c9e04cc8d17c67e.

--- app/src/main/java/com/andy/games/MainActivity.kt
+++ app/src/main/java/com/andy/games/MainActivity.kt
@@ -6,17 +6,28 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
-import androidx.compose.ui.platform.setContent
-
+import android.view.Menu
+import android.view.MenuItem
+import com.andy.games.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContent {
-            Text(text = "Hello")
+        setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 }
